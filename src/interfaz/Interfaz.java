7f5adc1d5443085beb75116315b5f9d6573c1647
N3ptune4197 <carloschero134@gmail.com>
2026-06-22 16:23:04 -0500

    Primer commit e inicio de proyecto

---
+++ src/interfaz/Interfaz.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author User
+ */
+public class Interfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Fabrizzio gay");
+    }
+    
+}
